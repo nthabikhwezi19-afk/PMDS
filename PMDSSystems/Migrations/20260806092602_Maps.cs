ebb677fe7b35cba8d89659771046204a0e281871
Dcs-Gito <Mapula.Lekganyane@dcs.gov.za>
2026-08-06 15:47:56 +0200

    Create

---
+++ PMDSSystems/Migrations/20260806092602_Maps.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PMDSSystems.Migrations
+{
+    /// <inheritdoc />
+    public partial class Maps : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
